0a3d39dba102416a08ce5f3405d000b193062094
Ali Mazhar <alimaza4444@gmail.com>
2023-12-17 19:14:47 +0500

    Make IT services section on landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import LandingPagehero from "@/components/LandingPage/LandingPageHero";
+import ITservices from "@/components/LandingPage/ITservices";
 
 export default function Home() {
   return (
     <div>
       <LandingPagehero />
+      <ITservices />
     </div>
   );
 }
