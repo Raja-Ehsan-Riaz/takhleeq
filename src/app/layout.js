ff7087bdc3cc0c5cb7083a5524deb650a41ebcbe
Ali Mazhar <alimaza4444@gmail.com>
2023-12-17 18:15:14 +0500

    Navbar developed

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,8 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Navbar /> */}
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
