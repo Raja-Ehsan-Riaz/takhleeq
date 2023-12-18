adb5135767779450475701f9564c2819febf702a
Ali Mazhar <alimaza4444@gmail.com>
2023-12-18 23:27:44 +0500

    Make Business Startup Pitch section - Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import LandingPagehero from "@/components/LandingPage/LandingPageHero";
 import ITservices from "@/components/LandingPage/ITservices";
+import BusinessStartupPitch from "@/components/LandingPage/BusinessStartupPitch";
 
 export default function Home() {
   return (
     <div>
       <LandingPagehero />
       <ITservices />
+      <BusinessStartupPitch />
     </div>
   );
 }
