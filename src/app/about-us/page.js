62fa19d3a32896c41d920b875b0c1e729e1f3860
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2024-01-27 21:34:33 +0500

    Added Hero Section

---
+++ src/app/about-us/page.js
@@ -0,0 +1,9 @@
+import AboutUsHero from "@/components/AboutUs/AboutUsHero";
+
+export default function Aboutus() {
+    return (
+        <div>
+            <AboutUsHero/>
+        </div>
+    );
+}
