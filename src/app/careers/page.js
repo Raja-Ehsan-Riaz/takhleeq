67d9472dba64bea46da59a5c2fe32d0d4fad7469
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2024-01-27 22:30:29 +0500

    Added Hero and Hire Section

---
+++ src/app/careers/page.js
@@ -0,0 +1,12 @@
+import CareerHero from "@/components/CareersPage/CareerHero";
+import TopSection from "@/components/CareersPage/TopSection";
+
+
+export default function Careers() {
+    return (
+        <div>
+            <CareerHero />
+            <TopSection/>
+        </div>
+    );
+}
