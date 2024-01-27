cb1f9feade0d73e94347b4b30a21152ea4586e0a
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2024-01-27 22:53:43 +0500

    Hero and FAQs added

---
+++ src/app/FAQs/page.js
@@ -0,0 +1,14 @@
+import FAQ from "@/components/FAQsPage/FAQ";
+import FAQhero from "@/components/FAQsPage/FAQhero";
+
+
+export default function FAQs() {
+    return (
+        <div>
+            <FAQhero />
+            <div className="flex justify-center items-center py-28 px-[5%]  bg-[#7957931A]">
+                <FAQ />
+            </div>
+        </div>
+    );
+}
