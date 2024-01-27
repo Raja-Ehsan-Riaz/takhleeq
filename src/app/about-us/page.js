ec3f1dcf6eb0c35ae0eba1139f0307ff1144b020
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2024-01-27 21:39:35 +0500

    Added Our Team Section

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,11 +1,13 @@
 import AboutUsHero from "@/components/AboutUs/AboutUsHero";
 import BrandingRevolution from "@/components/AboutUs/BrandingRevolution";
+import OurTeamContainer from "@/components/AboutUs/OurTeam/OurTeamContainer";
 
 export default function Aboutus() {
     return (
         <div>
             <AboutUsHero/>
             <BrandingRevolution/>
+            <OurTeamContainer/>
         </div>
     );
 }
