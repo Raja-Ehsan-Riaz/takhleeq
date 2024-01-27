d5aa7c6df472a1b39957d11a3b9245e28ed9902a
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2024-01-27 21:37:31 +0500

    Added Branding Revolution Section

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,9 +1,11 @@
 import AboutUsHero from "@/components/AboutUs/AboutUsHero";
+import BrandingRevolution from "@/components/AboutUs/BrandingRevolution";
 
 export default function Aboutus() {
     return (
         <div>
             <AboutUsHero/>
+            <BrandingRevolution/>
         </div>
     );
 }
