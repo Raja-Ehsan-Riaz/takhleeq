4ae82e59de5bb8899019fbdc14f9e70eb25b3cfe
Raja-Ehsan <rajaehsan.0022@gmail.com>
2024-10-22 12:57:04 +0500

    Video Added

--- src/components/LandingPage/LandingPageHero.jsx
+++ src/components/LandingPage/LandingPageHero.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
-import { list } from "@vercel/blob";
+// import { list } from "@vercel/blob";
 import Image from "next/image";
 
 const LandingPagehero = async () => {
-  const { blobs } = await list({
-    prefix: "PromoVideoTakhleeq",
-    limit: 1,
-  });
-  const { url } = blobs[0];
-  console.log(blobs);
+  // const { blobs } = await list({
+  //   prefix: "PromoVideoTakhleeq",
+  //   limit: 1,
+  // });
+  // const { url } = blobs[0];
+  // console.log(blobs);
 
   return (
     <div className="w-full h-[100vh] flex justify-center items-center bg-[#8838D3]/80 lg:bg-[#8838D3] text-white relative overflow-hidden ">
@@ -29,7 +29,7 @@ const LandingPagehero = async () => {
         preload="auto"
         loop
       >
-        <source src="https://res.cloudinary.com/dx6o0lsev/video/upload/v1720556230/PromoVideoTakhleeq_idppft.mp4" type="video/mp4" />
+        <source src="/Promo Video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="flex justify-center items-center z-30  lg:hidden">
