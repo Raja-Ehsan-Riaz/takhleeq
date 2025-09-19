a298f3cafb5c701d581f757464762e39617da0da
Raja-Ehsan-Riaz <rjanjua.bscs20seecs@seecs.edu.pk>
2025-09-20 00:00:37 +0500

    Services

--- src/components/LandingPage/Services.jsx
+++ src/components/LandingPage/Services.jsx
@@ -121,11 +121,6 @@ export default function ServicesSection() {
 						</div>
 					))}
 				</div>
-				<Link href={"/services"} className="mt-12">
-					<div className="px-12 py-4  focus:outline-none   relative border bg-white border-black text-black cursor-pointer font-bold text-sm transition-colors overflow-hidden before:absolute before:-right-[100%] before:top-0 before:z-10 before:h-[100rem] before:w-[200%] before:origin-top-right before:rotate-[15deg] before:hover:rotate-0 before:scale-x-50 before:bg-black before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100">
-						<div className="relative z-20">Explore More</div>
-					</div>
-				</Link>
 			</div>
 		</div>
 	)
